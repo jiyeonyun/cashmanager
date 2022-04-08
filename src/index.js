bb12df26ca6167e9d52e293624cab10e4c9248e1
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-08 12:33:14 +0900

    setting

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+function reducer(){
+  return [{id : 0, name : '멋진신발', quan : 2}]
+}
+let store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
